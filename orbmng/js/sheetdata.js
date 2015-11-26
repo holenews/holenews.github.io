c9e4cf89a55f64a3adf60c7b26405ce37a97c034
穴 <hole.news@gmail.com>
2015-11-26 23:27:21 +0900

    宝珠名を設定する処理を追加

--- orbmng/js/sheetdata.js
+++ orbmng/js/sheetdata.js
@@ -213,8 +213,7 @@
                 "</div>");
             var tabId = this.tabId;
             $(this.tabId + " .orb_list").append($row);
-            // 宝珠名称を設定する
-            $row.find(".orb_cell_name select").val(orb.p).change();
+            
             // 宝珠形状ボタンクリック
             $row.find(".orb_cell_img .orb_form").popover({
                 trigger: 'manual',
@@ -226,6 +225,8 @@
             $row.find(".orb_cell_level select").val(orb.p).change();
             // 宝珠名リストを作成する
             this.setOrbNameList($row.find(".orb_cell_name select"));
+            // 宝珠名称を設定する
+            $row.find(".orb_cell_name select").val(orb.p).change();
         },
 
         /**
